f1c3adcbb31aed5e5ff85e6924bafa1fb357e668
hGxL <henri.m.gil@gmail.com>
2018-06-25 13:58:40 +0200

    Multi thread server

--- main/server.c
+++ main/server.c
@@ -11,35 +11,44 @@
 #include "model/user.h"
 #include "model/user.c"
 
-int main() {
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <pthread.h>
+
+typedef int SOCKET;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+typedef struct in_addr IN_ADDR;
+
+pthread_cond_t      cond  = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t     mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int pthread_join(pthread_t th, void **thread_return);
+void pthread_exit(void *ret);
+int pthread_mutex_lock(pthread_mutex_t *mut);
+int pthread_mutex_unlock(pthread_mutex_t *mut);
+int pthread_mutex_destroy(pthread_mutex_t *mut);
+
+
+void * thread_server (void* c) {
+
+    printf("thread_server \n");
+
+    int rc = 0;
+    int socket = *((int*)c);
+    printf("sockect %d", socket);
 
     char str[100];
-    int listen_fd, conn_fd;
 
-    struct sockaddr_in servaddr;
+    presentRoutes();
 
-    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    while( 1 ) {
 
-    bzero( &servaddr, sizeof(servaddr));
-
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htons(INADDR_ANY);
-    servaddr.sin_port = htons(8081);
-
-    bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-
-    listen(listen_fd, 10);
-
-    conn_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
-
-    if(conn_fd > 1) {
-        presentRoutes();
-    }
-
-    while(1) {
+        rc = pthread_mutex_lock(&mutex);
 
         bzero( str, 100);
-        read(conn_fd,str,100);
+        read(socket,str,100);
 
         User random;
         random = randomUser();
@@ -49,9 +58,67 @@ int main() {
 
         res = userToString(random);
 
-        write(conn_fd, res, strlen(res)+1);
+        write(socket, res, strlen(res)+1);
 
         free(res);
+
+        rc = pthread_mutex_unlock(&mutex);
+        close(socket);
+    }
+
+    return 0;
+}
+
+
+int main() {
+
+    int socket_desc , client_sock , c ;
+    struct sockaddr_in server , client;
+
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1){
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8080 );
+
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("Bind failed");
+        return 1;
+    }
+
+    puts("Bind done");
+
+    //Listen
+    listen(socket_desc , 3); // 3 connection queue
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+
+    while (1) {
+
+        client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+        if (client_sock < 0)
+        {
+            perror("Accept failed");
+            return 1;
+        } else {
+
+            puts("\nConnection accepted");
+            pthread_t th1;
+            int rc = 0;
+            rc = pthread_create(&th1, NULL, thread_server, (void*)&client_sock);
+            rc = pthread_detach(th1);
+            printf("created\n");
+        }
     }
 }
 
